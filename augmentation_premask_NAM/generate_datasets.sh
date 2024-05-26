7721f5149530335d1ff9947a7580bb3fff3adc0c
LINGLONGQIAN <15869023990@163.com>
2024-05-26 23:07:59 +0100

    update

---
+++ augmentation_premask_NAM/generate_datasets.sh
@@ -0,0 +1,2 @@
+python data_processing/gene_physionet_2012.py
+python data_processing/gene_physionet_2012_seta.py
---
+++ overlay_premask/generate_datasets.sh
@@ -0,0 +1,4 @@
+python data_processing/gene_physionet_2012.py
+python data_processing/gene_physionet_2012_seta.py
+python data_processing/gene_air_quality.py
+python data_processing/gene_ettm1.py
