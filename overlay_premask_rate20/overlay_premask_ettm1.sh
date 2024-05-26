6e537df1cb94c28dc0f9b2cc690844a7852a0d7d
LINGLONGQIAN <15869023990@163.com>
2024-05-26 23:18:07 +0100

    update

---
+++ overlay_premask_rate20/overlay_premask_ettm1.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -l
+#SBATCH --output=/scratch/users/%u/ijcai24/overlay_premask/ettm1.log
+#SBATCH --gres=gpu:1
+#SBATCH --partition=biomed_a100_gpu
+#SBATCH --job-name=bash
+#SBATCH --mem=256G
+#SBATCH --mail-user=linglong.qian@kcl.ac.uk
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --time=2-00:00:00
+
+module load anaconda3/2021.05-gcc-13.2.0
+module load cuda/12.2.1-gcc-13.2.0
+module load cudnn/8.2.4.15-11.4-gcc-13.2.0
+
+nvidia-smi
+
+cat 1>&2 <<END
+task ${SLURM_JOB_ID}
+END
+
+cd /scratch/users/k1814348/ijcai24/overlay_premask
+source activate pypots
+
+python train_models_for_ettm1.py
